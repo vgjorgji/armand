528989bf04fb5dda91a48300626373751ad680b5
vgjorgji <vgjorgji@772657a8-2d39-4961-8a2c-0cfa3110e381>
2017-04-19 23:07:07 +0000

    ClientConfigurationService interface

---
+++ armand-cms-web-client/src/main/java/com/vcms/web/client/service/ClientConfigurationServiceImpl.java
@@ -0,0 +1,57 @@
+package com.vcms.web.client.service;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.vcms.website.model.Website;
+import com.vcms.website.model.WebsiteRepository;
+import com.vcms.website.model.WebsiteViewRepository;
+import com.vcms.website.model.WebsiteView;
+import com.vcms.website.model.WebsiteViewType;
+
+@Service
+public class ClientConfigurationServiceImpl implements ClientConfigurationService {
+
+	@Value("#{contextParameters['websiteName']}")
+	private String websiteName;
+	
+	@Autowired
+	private WebsiteRepository websiteRepository;
+	
+	@Autowired
+	private WebsiteViewRepository websiteVeiwRepository;
+	
+	private Website website;
+	private WebsiteView websiteView;
+	
+	@PostConstruct
+	public void init() {
+		website = websiteRepository.getWebsite(websiteName);
+		if (website == null) {
+			throw new IllegalStateException("No website for: " + websiteName);
+		}
+		websiteView = websiteVeiwRepository.getWebsiteView(website.getId(), WebsiteViewType.Live);
+		if (websiteView == null) {
+			throw new IllegalStateException("No website view for Live website :" + websiteName);
+		}
+	}
+	
+	@Override
+	public String getWebsiteName() {
+		return websiteName;
+	}
+
+	@Override
+	public Website getWebsite() {
+		return website;
+	}
+
+	@Override
+	public WebsiteView getWebsiteView() {
+		return websiteView;
+	}
+
+}
